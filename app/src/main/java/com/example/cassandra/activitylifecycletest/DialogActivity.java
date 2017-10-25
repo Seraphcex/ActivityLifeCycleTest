9996d895b59db1475108a99bb250a88b94d7a796
Seraphcex <caozehua926@hotmail.com>
2017-10-25 12:50:46 +0800

    Normal Update

--- app/src/main/java/com/example/cassandra/activitylifecycletest/DialogActivity.java
+++ app/src/main/java/com/example/cassandra/activitylifecycletest/DialogActivity.java
@@ -2,12 +2,50 @@ package com.example.cassandra.activitylifecycletest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class DialogActivity extends AppCompatActivity {
+    private static final String TAG = "DialogActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dialog_activity);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
     }
 }
